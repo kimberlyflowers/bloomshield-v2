380936d87939a78d9acca004f34d508f851c11f3
kimberlyflowers <42077154+kimberlyflowers@users.noreply.github.com>
2025-11-12 20:42:57 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>🌼 BloomShield V2</h1>
+      <p>Fresh Start - This Should Work!</p>
+    </div>
+  );
+}
