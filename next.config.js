0fc0206b6e2db8b884c76b6c196bde2e045e0f1f
kimberlyflowers <42077154+kimberlyflowers@users.noreply.github.com>
2025-11-13 12:52:44 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' thirdweb.com *.thirdweb.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
