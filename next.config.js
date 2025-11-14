00646729ccf1f53d7f30385acca12729be135ae6
kimberlyflowers <42077154+kimberlyflowers@users.noreply.github.com>
2025-11-14 13:02:05 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,15 +7,12 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' vercel.live; script-src 'self' 'unsafe-eval' 'unsafe-inline' vercel.live; style-src 'self' 'unsafe-inline' vercel.live; font-src 'self' data: vercel.live; img-src 'self' data: blob: vercel.live; connect-src 'self' https: wss: vercel.live;"
-          },
-        ],
-      },
-    ];
-  },
-  experimental: {
-    esmExternals: 'loose'
+            value: "default-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data:; connect-src 'self' https:;"
+          }
+        ]
+      }
+    ]
   }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
